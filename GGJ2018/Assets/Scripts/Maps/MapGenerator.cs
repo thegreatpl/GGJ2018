a57455a1cca72b57eb2c60b3f9ffffc09943239c
thegreatpl <imperialjustinian@gmail.com>
2018-01-26 20:38:35 +0000

    Added the basic map stuff.

---
+++ GGJ2018/Assets/Scripts/Maps/MapGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapGenerator : MonoBehaviour {
+
+    public Tilemap Base;
+
+    public Tilemap Walls;
+
+    public Tilemap Detail;
+
+
+    public int XSize = 100;
+
+    public int YSize = 100; 
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
