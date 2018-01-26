fefa5f3e4153c2e98be87aefed027c8c97628602
thegreatpl <imperialjustinian@gmail.com>
2018-01-26 21:01:44 +0000

    Basic map is done.

--- GGJ2018/Assets/Scripts/Maps/MapGenerator.cs
+++ GGJ2018/Assets/Scripts/Maps/MapGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -14,16 +15,41 @@ public class MapGenerator : MonoBehaviour {
 
     public int XSize = 100;
 
-    public int YSize = 100; 
+    public int YSize = 100;
+
+    /// <summary>
+    /// List of tiles that can be used to generate the map. 
+    /// </summary>
+    public List<TileStoreObj> Tiles = new List<TileStoreObj>(); 
 
 
 	// Use this for initialization
 	void Start () {
-		
+        GenerateMap();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
+    void GenerateMap()
+    {
+        var grasses = Tiles.Where(x => x.Type == "Grass");
+        var walls = Tiles.Where(x => x.Type == "Wall"); 
+
+        for (int xdx = 0; xdx <= XSize; xdx++)
+        {
+            for (int ydx = 0; ydx <= YSize; ydx++)
+            {
+                Base.SetTile(new Vector3Int(xdx, ydx, 0), grasses.ElementAt(Random.Range(0, grasses.Count())).tileBase);
+                if (xdx == 0 || ydx == 0 || XSize == xdx || YSize == ydx)
+                {
+                    Walls.SetTile(new Vector3Int(xdx, ydx, 0), walls.ElementAt(Random.Range(0, walls.Count())).tileBase); 
+                }
+
+            }
+        }
+    }
 }
---
+++ GGJ2018/Assets/Scripts/Maps/TileStoreObj.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[System.Serializable]
+public class TileStoreObj
+{
+    /// <summary>
+    /// Name of this tile. 
+    /// </summary>
+    public string Name;
+
+    /// <summary>
+    /// Type of this tile. 
+    /// </summary>
+    public string Type; 
+
+    /// <summary>
+    /// The tilebase. 
+    /// </summary>
+    public TileBase tileBase;
+
+}
