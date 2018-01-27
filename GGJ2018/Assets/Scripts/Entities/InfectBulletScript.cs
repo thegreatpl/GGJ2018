dea74d7a214080413739c1d6420529240c076602
thegreatpl <imperialjustinian@gmail.com>
2018-01-27 18:41:55 +0000

    We have bullets! The shoot. And then do nothing to civilians.

---
+++ GGJ2018/Assets/Scripts/Entities/InfectBulletScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfectBulletScript : MonoBehaviour {
+
+    public Vector3 Velocity = Vector3.zero;
+
+    public int Faction = 0; 
+
+    public int Life = 100;
+
+    /// <summary>
+    /// How fast the bullet flies. 
+    /// </summary>
+    public float BulletSpeed = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //kill it. 
+        //if (Life < 0)
+        //    Destroy(gameObject);
+
+        transform.position += Velocity; 
+
+        Life--; 
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var owner = collision.gameObject.GetComponent<EntityOwnership>();
+
+        if (owner == null)
+        {
+            Destroy(gameObject);
+            return; 
+        }
+
+        if (owner.Faction != Faction)
+        {
+            SpawnZombie(collision.gameObject); 
+        }
+        else
+        {
+            Destroy(gameObject); 
+            //Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>());
+        }
+
+    }
+
+    public void SpawnZombie(GameObject civilian)
+    {
+
+    }
+}
